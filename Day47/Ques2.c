d3203a9cc14ac6f10a4be1d46b48a4ffa4befdc0
SohilSaini75 <sohilsaini075@gmail.com>
2026-03-19 08:38:33 +0530

    Day47_Ques

---
+++ Day47/Ques2.c
@@ -0,0 +1,11 @@
+// Maximum Depth of Binary Tree
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int l = maxDepth(root->left);
+    int r = maxDepth(root->right);
+
+    return (l > r ? l : r) + 1;
+}
