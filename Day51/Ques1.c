349693bfad705000f03b08b48a84620e3aa4b9b8
SohilSaini75 <sohilsaini075@gmail.com>
2026-03-23 14:46:54 +0530

    Day51_Ques

---
+++ Day51/Ques1.c
@@ -0,0 +1,67 @@
+/*Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else if (val > root->data)
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+int findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if (n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root->data;
+    }
+    return -1;
+}
+
+int main() {
+    int n, x, a, b;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", findLCA(root, a, b));
+
+    return 0;
+}
---
+++ Day51/Ques2.c
@@ -0,0 +1,16 @@
+// LCA in BST
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root != NULL) {
+        if (p->val < root->val && q->val < root->val) {
+            root = root->left;
+        } 
+        else if (p->val > root->val && q->val > root->val) {
+            root = root->right;
+        } 
+        else {
+            return root;
+        }
+    }
+    return NULL;
+}
