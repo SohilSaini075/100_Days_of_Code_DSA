76231a4898d5a483d8b9a16a4edbd4ee02388952
SohilSaini75 <sohilsaini075@gmail.com>
2026-03-24 08:27:10 +0530

    Day52_Ques

---
+++ Day52/Ques1.c
@@ -0,0 +1,88 @@
+/*Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* nodes[n];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = createNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    return nodes[0];
+}
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* leftLCA = findLCA(root->left, n1, n2);
+    struct Node* rightLCA = findLCA(root->right, n1, n2);
+
+    if (leftLCA && rightLCA)
+        return root;
+
+    return (leftLCA != NULL) ? leftLCA : rightLCA;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    struct Node* root = buildTree(arr, n);
+    struct Node* lca = findLCA(root, a, b);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
