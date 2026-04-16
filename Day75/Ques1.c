0e63c7369850836c9e3807c8b33f6b138761175e
SohilSaini75 <sohilsaini075@gmail.com>
2026-04-16 10:21:28 +0530

    Day75_Ques

---
+++ Day75/Ques1.c
@@ -0,0 +1,42 @@
+/*Problem Statement
+Given an array of integers containing both positive and negative values, find the length of the longest contiguous subarray whose sum is equal to zero.
+
+Input Format
+An integer array arr[].
+
+Output Format
+Print the length of the longest subarray with sum equal to zero.*/
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int longestZeroSumSubarray(int arr[], int n) {
+    unordered_map<int, int> mp;
+    int sum = 0, maxLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+
+        if (sum == 0) {
+            maxLen = i + 1;
+        }
+
+        if (mp.find(sum) != mp.end()) {
+            maxLen = max(maxLen, i - mp[sum]);
+        } else {
+            mp[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main() {
+    int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << longestZeroSumSubarray(arr, n);
+
+    return 0;
+}
